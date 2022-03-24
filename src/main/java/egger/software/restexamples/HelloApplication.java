4462ab1b0f5848c4d5fb4caf10cdb1d16e6f5687
Alexander Egger <egger@csdisco.com>
2022-03-24 09:21:41 +0100

    initial

---
+++ src/main/java/egger/software/restexamples/HelloApplication.java
@@ -0,0 +1,9 @@
+package egger.software.restexamples;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/egger/software/restexamples/Main.java
@@ -0,0 +1,41 @@
+package egger.software.restexamples;
+
+import org.eclipse.jetty.server.Server;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        
+        try {
+            URI baseUri = UriBuilder.fromUri("http://localhost").port(8090).build();
+            ResourceConfig config = new ResourceConfig(HelloResource.class);
+            Server server = JettyHttpContainerFactory.createServer(baseUri, config);
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                try {
+                    System.out.println("Shutting down the server...");
+                    server.stop();
+                    System.out.println("Done.");
+                } catch (Exception ex) {
+                    logger.log(Level.SEVERE, ex.getMessage(), ex);
+                }
+            }));
+
+            System.out.println("Server started.\nCTRL+C to exit.");
+            server.join();
+
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+
+    }
+
+}
